4b9b9e53db3ddb138f57412a1c485406a46425d4
logued <logued@dkit.ie>
2026-02-23 13:45:38 +0000

    small updates

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main() {
     std::cout << "Image class manipulation" << std::endl;
 
-    Image image1(30,20);
+    Image image1(6,4);
     // create an Image object, with allocated memory for image pixel data
     // note that when the data vector (of RGB structs) is created,
     // the elements in the vector are not initialized (saving time).
@@ -13,12 +13,12 @@ int main() {
     image1.at(0,0) = RGB(255,255,255);
     std::cout << "Red pixel value: " << static_cast<int>(image1.at(1,1).r) << std::endl;
 
-    // image1.at(0,1) = RGB(255,0,0);
+     image1.at(1,0) = RGB(255,0,0);
     //
 
     //image1.at(666,2) = RGB(0,255,0);
 
-     for (auto iter =  image1.begin(); iter != image1.end(); iter++) {
+     for (auto iter =  image1.begin(); iter != image1.end(); ++iter) {
          std::cout << static_cast<int>(iter->r) << " "
                     << static_cast<int>(iter->g) << " "
                     << static_cast<int>(iter->b) << ", ";
